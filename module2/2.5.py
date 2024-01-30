d782a74d31eb55c4a9dba992cfe8ea134fbe28f2
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-01-30 15:40:51 +0200

    all tasks of module 2 have been completed

---
+++ module2/2.5.py
@@ -0,0 +1,16 @@
+# Примітка, вставлена в програму, яка пропускається під час виконання, називається коментарем.
+
+# uncomment_me = 1
+# uncomment_me_too = 3
+# uncomment_me_also = 5
+
+# print(uncomment_me, uncomment_me_too, uncomment_me_also, sep="\n")
+
+
+#ця програма обчислює кількість секунд в заданій кількості годин
+
+# hours = 2 
+# seconds = 3600
+
+# print("Годин: ", hours) 
+# print("Секунд в годинах: ", hours * seconds) 
